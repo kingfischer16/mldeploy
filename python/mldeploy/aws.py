8be120873c1d8aa9d96091985d01c85d50b86da0
Lee <lee.m.fischer@gmail.com>
2020-12-08 21:00:31 +0100

    started AWS Python file

---
+++ python/mldeploy/aws.py
@@ -0,0 +1,13 @@
+# =============================================================================
+# AWS.PY
+# -----------------------------------------------------------------------------
+# Functions used to interact with Amazon Web Services.
+# 
+# ***This file may only import from the 'utils' file.***
+# 
+#  
+# The CLI is built using the following packages:
+#   - boto3: Python SDK for AWS
+# -----------------------------------------------------------------------------
+# Author: kingfischer16 (https://github.com/kingfischer16/mldeploy)
+# =============================================================================
