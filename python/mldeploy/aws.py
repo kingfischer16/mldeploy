58353e089a4ca7c1e8e44438800460b73c97c4ea
Lee <lee.m.fischer@gmail.com>
2020-12-09 21:16:19 +0100

    added function templates

--- python/mldeploy/aws.py
+++ python/mldeploy/aws.py
@@ -16,6 +16,8 @@
 # Imports.
 # -----------------------------------------------------------------------------
 import boto3
+from datetime import datetime
+from typing import NoReturn
 from .utils import _add_field_to_registry
 
 
@@ -24,4 +26,22 @@ from .utils import _add_field_to_registry
 # -----------------------------------------------------------------------------
 def _create_cloudformation_file(name: str) -> NoReturn:
     """
-    Creates and registers
+    Creates and registers a CloudFormation JSON file.
+
+    Args:
+        name (str): Name of the project to create the cloudformation file.
+    """
+
+
+def _create_stack_id(name: str) -> str:
+    """
+    Creates a stack name that should be unique within the region.
+
+    Args:
+        name (str): Name of the project for which to create the stack ID.
+    
+    Returns:
+        (str): The generated stack name.
+    """
+    dt_string = datetime.now().strftime("%Y%m%d-%H%M%S")
+    return name + '_' + dt_string
