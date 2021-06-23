6e18f7a5447ed35628c7790bf1de93c3cbdeaaaa
Lee <lee.m.fischer@gmail.com>
2021-06-23 16:48:45 +0200

    started rebuild of aws.py, new method without altering template files (parameter passing only)

--- python/mldeploy/aws.py
+++ python/mldeploy/aws.py
@@ -17,6 +17,7 @@
 # -----------------------------------------------------------------------------
 import boto3
 from datetime import datetime
+import io
 import json
 import ruamel.yaml as ryml  # Allows modification of YAML file without disrupting comments.
 import os
@@ -30,34 +31,174 @@ from .utils import (
     _get_constant,
 )
 
-
 # =============================================================================
-# CloudFormation template creation function, top level.
+# Deployment control.
 # -----------------------------------------------------------------------------
-def _add_cloudformation_template(name: str) -> NoReturn:
+def _deploy_stack(name: str) -> NoReturn:
     """
-    Adds the CloudFormation template to the configuration folder.
+    Deploys the stack to AWS for the given project using CloudFormation.
 
-    This function should be called during the build phase.
+    Deploying the stack should consist of:
+        - creating the temporary S3 bucket for template upload
+        - upload docker image (stage 2)
+        - uploading the template files
+        - get parameters from config: S3 data bucket, cluster size, EC2 type,
+        - create the master stack
+        - wait for the stack creation to finish --> "waiter"
+        - register: queue url, api key, api key
+        - remove temporary S3 bucket (if not being used for docker repo)
 
     Args:
-        name (str): Name of the project to create the cloudformation file.
+        name (str): The project name.
     """
-    if os.path.exists(
-        _get_field_if_exists(name, _get_constant("CLOUDFORMATION_LOCATION_KEY"))
-    ):
+    deployed_status = _get_field_if_exists(name, _get_constant("DEPLOY_STATUS_KEY"))
+    if deployed_status == _get_constant("STATUS_DEPLOYED"):
         print(
-            f"{_get_constant('MSG_PREFIX')}CloudFormation template already exists for project '{name}'. Existing template was not overwritten."
+            f"{_get_constant('FAIL_PREFIX')}Deployment failed for project '{name}. A stack is already deployed for this project."
         )
-    else:
-        _create_cloudformation_file(name)
-        _add_project_s3_bucket(name)
-        _add_ec2_instance(name)
+        return
+    # Prepare parameters for stack creation.
+    master_stack_name = f"mldeploy-{name}"
+    s3_stack_name, s3_bucket_name = _deploy_s3_bucket(name)
+
+    # Get parameters from config file.
+
+    # Create
+    yaml_obj = ryml.YAML()
+    cf_filepath = _get_field_if_exists(
+        name, _get_constant("CLOUDFORMATION_LOCATION_KEY")
+    )
+    with open(cf_filepath, "r") as f:
+        cf_template_yaml = yaml_obj.load(f)
+    cf_template = json.dumps(cf_template_yaml)
+    # Create client.
+    client = boto3.client("cloudformation")
+    # Create stack.
+    d_stack_id = client.create_stack(StackName=stack_name, TemplateBody=cf_template)
+    stack_id = d_stack_id["StackId"]
+    print(
+        f"{_get_constant('MSG_PREFIX')}Deployment created successfully for project '{name}'.\n\tStack ID: {stack_id}"
+    )
+    # Register stack.
+    _register_deployment(name, stack_name, stack_id, deployed=True)
+
+
+def _undeploy_stack(name: str) -> NoReturn:
+    """
+    Removes a deployed stack from AWS CloudFormation.
+
+    Args:
+        name (str): The project name.
+    """
+    deployed_status = _get_field_if_exists(name, _get_constant("DEPLOY_STATUS_KEY"))
+    if deployed_status == _get_constant("STATUS_NOT_DEPLOYED"):
+        print(
+            f"{_get_constant('FAIL_PREFIX')}Undeploy action failed for project '{name}. Project has no stack deployed."
+        )
+        return
+    # Create stack name.
+    stack_name = _get_field_if_exists(name, _get_constant("STACK_NAME_KEY"))
+    # Create client.
+    client = boto3.client("cloudformation")
+    # Create stack.
+    client.delete_stack(StackName=stack_name)
+    print(f"{_get_constant('MSG_PREFIX')}Stack removed for project '{name}'.")
+    # Register stack.
+    _register_deployment(name, "", "", deployed=False)
+
+
+def _register_deployment(
+    name: str, stack_name: str, stack_id: str, deployed: bool
+) -> NoReturn:
+    """
+    Registers or deregisteres the stack ID of the deployment in
+    the project registry and sets the 'deployment_status' of the project.
+
+    Args:
+        name (str): The project name.
+
+        stack_name (str): The name of the CloudFormation stack to register.
+
+        stack_id (str): The CloudFormation ID of the deployed stack.
+
+        deployed (bool): Set True if stack is deployed and being registered,
+         set False if the stack is being removed and being deregistered.
+    """
+    _add_field_to_registry(name, _get_constant("STACK_NAME_KEY"), stack_name)
+    _add_field_to_registry(name, _get_constant("STACK_ID_KEY"), stack_id)
+    deploy_status = (
+        _get_constant("STATUS_DEPLOYED")
+        if deployed
+        else _get_constant("STATUS_NOT_DEPLOYED")
+    )
+    _add_field_to_registry(name, _get_constant("DEPLOY_STATUS_KEY"), deploy_status)
 
 
 # =============================================================================
-# Architecture setup.
+# Deployment subfunctions.
 # -----------------------------------------------------------------------------
+def _deploy_s3_bucket(name: str) -> str:
+    """
+    Deploys a S3 bucket to contain the templates for the CloudFormation
+    deployment.
+
+    Args:
+        name (str): The project name.
+
+    Returns:
+        (str): The S3 bucket stack name and bucket name.
+    """
+    s3_stack_name = f"mldeploy-s3-templates-{name}"
+    bucket_name = f"mldeploy-s3-{name}"
+
+    yaml_obj = ryml.YAML()
+    yaml_obj.preserve_quotes = True
+    template_str = io.StringIO()
+
+    cf_filepath = "./deploy_templates/create_s3_bucket.yml"
+
+    with open(cf_filepath, "r") as f:
+        cf_template_yaml = yaml_obj.load(f)
+
+    yaml_obj.dump(cf_template_yaml, template_str)
+    cf_template = template_str.getvalue()
+
+    cfn_client = boto3.client("cloudformation")
+    cfn_stack_create_complete_waiter = cfn_client.get_waiter(
+        waiter_name="stack_create_complete"
+    )
+    d_s3_stack_id = cfn_client.create_stack(
+        StackName=s3_stack_name,
+        TemplateBody=cf_template,
+        Parameters=[
+            {"ParameterKey": "ProjectName", "ParameterValue": name},
+            {"ParameterKey": "S3BucketName", "ParameterValue": bucket_name},
+        ],
+    )
+    cfn_stack_create_complete_waiter.wait(StackName=s3_stack_name)
+
+    print(
+        f"{_get_constant('MSG_PREFIX')}S3 Bucket successfully deployed. Stack Id: {d_s3_stack_id['StackId']}"
+    )
+    return s3_stack_name, bucket_name
+
+
+def _upload_files_to_s3(bucket_name: str, files_dict: dict) -> NoReturn:
+    """
+    Uploads a series of files to a specified S3 bucket.
+
+    Args:
+        bucket_name (str): The S3 bucket name.
+
+        files_dict (dict): Key-value pairs of the form:
+            - key: desired file name on S3
+            - value: complete file path and name on local machine
+    """
+    s3_client = boto3.client("s3")
+    for filename, filepath in files_dict.items():
+        s3_client.upload_file(filepath, bucket_name, filename)
+
+
 def _create_cloudformation_file(name: str) -> NoReturn:
     """
     ### CHANGE TO METHOD - NO MORE CHANGING TEMPLATE FILE ###
@@ -150,6 +291,30 @@ def _add_ec2_instance(name: str) -> NoReturn:
 
 
 # =============================================================================
+# CloudFormation template creation function, top level.
+# -----------------------------------------------------------------------------
+def _add_cloudformation_template(name: str) -> NoReturn:
+    """
+    Adds the CloudFormation template to the configuration folder.
+
+    This function should be called during the build phase.
+
+    Args:
+        name (str): Name of the project to create the cloudformation file.
+    """
+    if os.path.exists(
+        _get_field_if_exists(name, _get_constant("CLOUDFORMATION_LOCATION_KEY"))
+    ):
+        print(
+            f"{_get_constant('MSG_PREFIX')}CloudFormation template already exists for project '{name}'. Existing template was not overwritten."
+        )
+    else:
+        _create_cloudformation_file(name)
+        _add_project_s3_bucket(name)
+        _add_ec2_instance(name)
+
+
+# =============================================================================
 # CloudFormation template manipulations.
 # -----------------------------------------------------------------------------
 def _get_cloudformation_template_data(name: str) -> Dict:
@@ -208,105 +373,6 @@ def _update_cloudformation_template_data(name: str, data: Dict) -> NoReturn:
     yaml_obj = ryml.YAML()
     with open(cf_filepath, "w") as f:
         yaml_obj.dump(data, f)
-
-
-# =============================================================================
-# Deployment control.
-# -----------------------------------------------------------------------------
-def _deploy_stack(name: str) -> NoReturn:
-    """
-    Deploys the stack to AWS for the given project using CloudFormation.
-
-    Deploying the stack should consist of:
-        - creating the temporary S3 bucket for template upload
-        - upload docker image (stage 2)
-        - uploading the template files
-        - get parameters from config: S3 data bucket, cluster size, EC2 type,
-        - create the master stack
-        - wait for the stack creation to finish --> "waiter"
-        - register: queue url, api key, api key
-        - remove temporary S3 bucket (if not being used for docker repo)
-
-    Args:
-        name (str): The project name.
-    """
-    deployed_status = _get_field_if_exists(name, _get_constant("DEPLOY_STATUS_KEY"))
-    if deployed_status == _get_constant("STATUS_DEPLOYED"):
-        print(
-            f"{_get_constant('FAIL_PREFIX')}Deployment failed for project '{name}. A stack is already deployed for this project."
-        )
-        return
-    # Create stack name.
-    stack_name = f"mldeploy-{name}"
-    # Get template.
-    yaml_obj = ryml.YAML()
-    cf_filepath = _get_field_if_exists(
-        name, _get_constant("CLOUDFORMATION_LOCATION_KEY")
-    )
-    with open(cf_filepath, "r") as f:
-        cf_template_yaml = yaml_obj.load(f)
-    cf_template = json.dumps(cf_template_yaml)
-    # Create client.
-    client = boto3.client("cloudformation")
-    # Create stack.
-    d_stack_id = client.create_stack(StackName=stack_name, TemplateBody=cf_template)
-    stack_id = d_stack_id["StackId"]
-    print(
-        f"{_get_constant('MSG_PREFIX')}Deployment created successfully for project '{name}'.\n\tStack ID: {stack_id}"
-    )
-    # Register stack.
-    _register_deployment(name, stack_name, stack_id, deployed=True)
-
-
-def _undeploy_stack(name: str) -> NoReturn:
-    """
-    Removes a deployed stack from AWS CloudFormation.
-
-    Args:
-        name (str): The project name.
-    """
-    deployed_status = _get_field_if_exists(name, _get_constant("DEPLOY_STATUS_KEY"))
-    if deployed_status == _get_constant("STATUS_NOT_DEPLOYED"):
-        print(
-            f"{_get_constant('FAIL_PREFIX')}Undeploy action failed for project '{name}. Project has no stack deployed."
-        )
-        return
-    # Create stack name.
-    stack_name = _get_field_if_exists(name, _get_constant("STACK_NAME_KEY"))
-    # Create client.
-    client = boto3.client("cloudformation")
-    # Create stack.
-    client.delete_stack(StackName=stack_name)
-    print(f"{_get_constant('MSG_PREFIX')}Stack removed for project '{name}'.")
-    # Register stack.
-    _register_deployment(name, "", "", deployed=False)
-
-
-def _register_deployment(
-    name: str, stack_name: str, stack_id: str, deployed: bool
-) -> NoReturn:
-    """
-    Registers or deregisteres the stack ID of the deployment in
-    the project registry and sets the 'deployment_status' of the project.
-
-    Args:
-        name (str): The project name.
-
-        stack_name (str): The name of the CloudFormation stack to register.
-
-        stack_id (str): The CloudFormation ID of the deployed stack.
-
-        deployed (bool): Set True if stack is deployed and being registered,
-         set False if the stack is being removed and being deregistered.
-    """
-    _add_field_to_registry(name, _get_constant("STACK_NAME_KEY"), stack_name)
-    _add_field_to_registry(name, _get_constant("STACK_ID_KEY"), stack_id)
-    deploy_status = (
-        _get_constant("STATUS_DEPLOYED")
-        if deployed
-        else _get_constant("STATUS_NOT_DEPLOYED")
-    )
-    _add_field_to_registry(name, _get_constant("DEPLOY_STATUS_KEY"), deploy_status)
 
 
 # ============================================================================
